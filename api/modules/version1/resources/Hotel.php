58f7576a15aeec12d12fdf13e0eba53bf2ddc254
Apple <farhad9>
2023-03-08 23:12:21 +0500

    api: relational expand added

--- api/modules/version1/resources/Hotel.php
+++ api/modules/version1/resources/Hotel.php
@@ -13,7 +13,7 @@ class Hotel extends \common\models\Hotels implements Linkable
 {
     public function extraFields()
     {
-        return [];
+        return ['hotelImages','hotelRooms','hotelStars','hotelViews','meals','facilities'];
     }
     /**
      * Returns a list of links.
