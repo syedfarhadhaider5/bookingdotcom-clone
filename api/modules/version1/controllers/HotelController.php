72743651cd866a69bfe9c57799fdd2083b90840f
Apple <farhad9>
2023-03-26 13:17:52 +0500

    api: hotel by name or country added

--- api/modules/version1/controllers/HotelController.php
+++ api/modules/version1/controllers/HotelController.php
@@ -31,5 +31,16 @@ class HotelController extends ActiveController
             return ['error' => 'record not found'];
         }
     }
+    public function actionFindByNameOrCountry()
+    {
+        $q = \Yii::$app->request->get('q');
+        //,hotelRooms,hotelStars,hotelViews,meals,facilities,hotelPrices
+        $result =Hotels::find()->with('hotelImages')->with('hotelMeals')->with('hotelRooms')->with('hotelStars')->with('hotelViews')->with('meals')->with('facilities')->with('hotelPrices')->where(['=','country_slug', $q])->orWhere(['=','name_slug', $q])->all();
+        if (count($result) > 0) {
+            return $result;
+        } else {
+            return ['error' => 'record not found'];
+        }
+    }
 
 }
